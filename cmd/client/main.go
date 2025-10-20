c5b0070c6494cce63f3f31b8791cf57e4c3fa3dd
aleksaelezovic <aleksaelezovic@gmail.com>
2025-10-20 15:27:24 +0200

    client: bind to the queue

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -5,6 +5,9 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -16,6 +19,24 @@ func main() {
 	}
 	fmt.Println("Successfully connected to rabbitmq.")
 	defer conn.Close()
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		fmt.Printf("Error getting username: %s", err.Error())
+		os.Exit(1)
+	}
+
+	_, _, err = pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilDirect,
+		routing.PauseKey+"."+username,
+		routing.PauseKey,
+		pubsub.Transient,
+	)
+	if err != nil {
+		fmt.Printf("Error declaring and binding queue: %s", err.Error())
+		os.Exit(1)
+	}
 
 	signalCh := make(chan os.Signal, 1)
 	signal.Notify(signalCh, os.Interrupt)
