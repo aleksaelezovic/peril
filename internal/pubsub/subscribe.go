dcbd65be187ca9e2b5167d779aabd2f388237803
aleksaelezovic <aleksaelezovic@gmail.com>
2025-10-20 15:57:19 +0200

    add subscribing logic

--- internal/pubsub/subscribe.go
+++ internal/pubsub/subscribe.go
@@ -1,41 +1,40 @@
 package pubsub
 
 import (
+	"encoding/json"
+	"log"
+
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-type SimpleQueueType int
-
-const (
-	Durable SimpleQueueType = iota
-	Transient
-)
-
-func DeclareAndBind(
+func SubscribeJSON[T any](
 	conn *amqp.Connection,
 	exchange,
 	queueName,
 	key string,
 	queueType SimpleQueueType, // an enum to represent "durable" or "transient"
-) (*amqp.Channel, amqp.Queue, error) {
-	ch, err := conn.Channel()
+	handler func(T),
+) error {
+	ch, q, err := DeclareAndBind(conn, exchange, queueName, key, queueType)
 	if err != nil {
-		return nil, amqp.Queue{}, err
+		return err
 	}
-	q, err := ch.QueueDeclare(
-		queueName,
-		queueType == Durable,
-		queueType == Transient,
-		queueType == Transient,
-		false,
-		nil,
-	)
+	deliveriesCh, err := ch.Consume(q.Name, "", false, false, false, false, nil)
 	if err != nil {
-		return nil, amqp.Queue{}, err
+		return err
 	}
-	err = ch.QueueBind(q.Name, key, exchange, false, nil)
-	if err != nil {
-		return nil, amqp.Queue{}, err
-	}
-	return ch, q, nil
+	go func() {
+		for delivery := range deliveriesCh {
+			var msg T
+			if err := json.Unmarshal(delivery.Body, &msg); err != nil {
+				log.Printf("failed to unmarshal message: %v", err)
+			} else {
+				handler(msg)
+			}
+			if err := delivery.Ack(false); err != nil {
+				log.Printf("failed to ack message: %v", err)
+			}
+		}
+	}()
+	return nil
 }
