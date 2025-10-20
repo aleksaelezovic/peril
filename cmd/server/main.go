5f244b02cd8f671045e34674ab643d0c39b596aa
aleksaelezovic <aleksaelezovic@gmail.com>
2025-10-20 15:16:47 +0200

    publish json message to a queue

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -14,9 +16,24 @@ func main() {
 		fmt.Printf("Error connecting to rabbitmq: %s", err.Error())
 		os.Exit(1)
 	}
+	fmt.Println("Successfully connected to rabbitmq.")
 	defer conn.Close()
 
-	fmt.Println("Successfully connected to rabbitmq.")
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Printf("Error creating channel: %s", err.Error())
+		os.Exit(1)
+	}
+	defer ch.Close()
+
+	err = pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+		IsPaused: true,
+	})
+	if err != nil {
+		fmt.Printf("Error publishing message: %s", err.Error())
+		os.Exit(1)
+	}
+
 	signalCh := make(chan os.Signal, 1)
 	signal.Notify(signalCh, os.Interrupt)
 	<-signalCh
