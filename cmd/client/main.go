8bc9bc542fc4413e1a1e2209a7dd42b98834510a
aleksaelezovic <aleksaelezovic@gmail.com>
2025-10-20 15:17:54 +0200

    connect client to rabbitmq

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril client...")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		fmt.Printf("Error connecting to rabbitmq: %s", err.Error())
+		os.Exit(1)
+	}
+	fmt.Println("Successfully connected to rabbitmq.")
+	defer conn.Close()
+
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt)
+	<-signalCh
+	fmt.Println("Shutting down...")
 }
